6e9e23c305d8c2b238a8522af6b1f604fcfbd94e
Tait Hoyem <tait@tait.tech>
2020-06-18 03:04:27 +0000

    Rewrite

--- client/client.js
+++ client/client.js
@@ -1,26 +1,24 @@
-const MOVING_KEYS = [
-  {"keys": ["w", "ArrowUp"], val: "north"},
-  {"keys": ["d", "ArrowRight"], val: "east"},
-  {"keys": ["s", "ArrowDown"], val: "south"},
-  {"keys": ["a", "ArrowLeft"], val: "west"},
-];
-
-var Modes = {
-    NORMAL: "Normal",
-    MOVE: "Move",
-    LOOK: "Look"
-};
-var mode = Modes.NORMAL;
-var myname = window.prompt("What is your name: ", "Joe Blow");
 var sock = io();
 var sockid;
-// if unsucessful
-if (myname == null || myname == "") {
-    alert("This may not work.... You must reload and specify a name.");
-}
-else {
-    // if successful
-}
+
+var accuse_info = {};
+
+var Modes = {
+  NORMAL: 0,
+  PERSON: 1,
+  PLACE: 2,
+  THING: 3
+};
+var mode = Modes.NORMAL;
+
+const accuse = () => {
+  sock.emit("accuse", JSON.stringify({person: accuse_info.person, place: accuse_info.place, thing: accuse_info.thing}));
+};
+
+const move = (dir) => {
+  sock.emit("move", JSON.stringify({direction: dir}));
+};
+
 const writeLog = (res) => {
     var log = document.getElementById("statusbox");
     log.innerHTML += res + "<br>";
@@ -31,104 +29,81 @@ const printLog = (res) => {
     log.innerHTML += res;
     log.scrollTop = log.scrollHeight;
 };
-const move_me = (text) => {
-    console.log(text);
-    writeLog(text);
-};
-const update_other = (text) => {
-    writeLog(text);
-};
-const debug = (text) => {
-    console.log(text);
-};
-const set_users = (text) => {
-    writeLog(text);
-};
-const add_new_user = (text) => {
-    writeLog(text + " has joined the game");
-};
-const rem_disco_user = (text) => {
-    writeLog(text + " has left the game");
-};
-const setid = (text) => {
-    sockid = text;
-};
-const key_to_direction = (key) => {
-  for (kv_pair of MOVING_KEYS){
-    if (kv_pair.keys.includes(key)){
-      return kv_pair.val;
-    }
+
+const nMode = (e) => {
+  switch (e.key)
+  {
+    case "a":
+    case "A":
+      move("left");
+      break;
+    case "d":
+    case "D":
+      move("right");
+      break;
+    case "c":
+    case "C":
+      mode = Modes.PERSON;
+      sock.emit("people-req", {});
+      break;
+    default:
+      break;
   }
 };
 
-const key_handler = (e) => {
-    e.preventDefault();
-    console.log(e.key);
-    if (e.key === "F1" || e.key === "F2") {
-        writeLog("escape to return to normal mode");
-        writeLog("c to view current coordinates");
-        writeLog("m to go into move mode");
-        writeLog(">a to move left");
-        writeLog(">d to move right");
-        writeLog(">w to move up");
-        writeLog(">s to move down");
-        writeLog("");
-    }
-    if (e.key === "escape") {
-        mode = Modes.NORMAL;
-        writeLog("You are in normal mode.");
-        return;
-    }
-    if (e.key === "r") {
-        sock.emit("roll", "");
-        return;
-    }
-    if (e.key === "c") {
-        sock.emit("request-location");
-        return;
-    }
-    if (e.key === "l") {
-      mode = Modes.LOOK;
-      writeLog("You are in look-around mode.");
-      return;
-    }
-    if (e.key === "m") {
-        mode = Modes.MOVE;
-        writeLog("You are in moving mode");
-        return;
-    }
-    if (mode === Modes.LOOK) {
-      const kd = key_to_direction(e.key);
-      if (kd){
-        sock.emit("look", JSON.stringify({"name": myname, "id": sockid,"direction": kd}));
-      }
-    }
-    if (mode === Modes.MOVE) {
-      const kd = key_to_direction(e.key);
-      if (kd){
-        sock.emit("move", JSON.stringify({"name": myname, "id": sockid, "direction": kd}));
-      }
-    }
+const personMode = (e) => {
+  accuse_info.person = e.key;
+  console.log("Person: " + e.key);
+  sock.emit("places-req", {});
+  mode = Modes.PLACE;
 };
-const move_failed = (resp) => {
-    writeLog(resp);
+
+const placeMode = (e) => {
+  accuse_info.place = e.key;
+  console.log("Place: " + e.key);
+  sock.emit("things-req", {});
+  mode = Modes.THING;
 };
-sock.on("request-location-suc", writeLog);
-sock.on("welcome", writeLog);
-sock.on("update", update_other);
-sock.on("self-update", move_me);
-sock.on("debug", debug);
-sock.on("new-player", add_new_user);
-sock.on("already-active-users", set_users);
-sock.on("user-left", rem_disco_user);
-sock.on("joined", setid);
-sock.on("move-failed", move_failed);
-sock.on("client-error", writeLog);
-sock.on("location-info", writeLog);
-sock.on("your-move", writeLog);
-sock.on("roll-res", writeLog);
+
+const thingMode = (e) => {
+  accuse_info.thing = e.key;
+  mode = Modes.NORMAL;
+  console.log("Thing: " + e.key);
+  accuse();
+};
+
+const keyHandle = (e) => {
+  switch (mode)
+  {
+    case Modes.NORMAL:
+      nMode(e);
+      break;
+    case Modes.PERSON:
+      personMode(e);
+      break;
+    case Modes.PLACE:
+      placeMode(e);
+      break;
+    case Modes.THING:
+      thingMode(e);
+      break;
+  }
+};
+
+const print_options_set_valid = (l) => {
+  let jl = JSON.parse(l);
+  console.log(jl);
+  for (var i = 0; i < jl.length; i++)
+  {
+    writeLog(i + ": " + jl[i]);
+  }
+};
+
+sock.on("people-res", print_options_set_valid);
+sock.on("places-res", print_options_set_valid);
+sock.on("things-res", print_options_set_valid);
 
 window.onload = () => {
-    sock.emit("register-user", myname);
-    window.addEventListener("keyup", key_handler);
+  writeLog("Hello");
+  window.addEventListener("keydown", keyHandle);
 };
--- server/game.js
+++ server/game.js
@@ -1,35 +1,87 @@
-"use strict";
-
-class Player {
-  constructor(n_socketid){
-    this._socketid = n_socketid;
-    this._name = "Unknown";
-    this._health = 10;
-  }
-  get name(){
-    return this._name;
-  }
-  set name(n_name){
-    this._name = n_name;
-  }
-
-  get socketid(){
-    return this._socketid;
-  }
-  set socketid(n_sockid){
-    this._socketid = n_sockid;
-  }
-
-  get health(){
-    return this.health;
-  }
-  set health(n_health){
-    this._health = n_health;
-  }
-};
-
 class Game {
-  constructor(){
-    
+  constructor()
+  {
+    this.players = {};
+    this.acusations = [];
+    this.all_characters = ["Colonol Mustard", "Professor Plumb", "Scarlet"];
+    this.weapons = ["Wrench", "Pipe", "Rope", "Revolver"];
+    this.rooms = ["Library", "Study", "Bathroom"];
+    this.characters = ["Colonol Mustard", "Professor Plumb", "Scarlet"];
+    this.used_characters = [];
+  }
+  // uses the index of the items
+  accuse(person_i, place_i, thing_i)
+  {
+    let person = this.all_characters[person_i];
+    let place = this.rooms[place_i];
+    let thing = this.weapons[thing_i];
+    let acc = {person: person, place: place, thing: thing};
+    this.acusations.push(acc);
+  }
+  getAccusations()
+  {
+    return this.acusations;
+  }
+  getPlayers()
+  {
+    return this.players;
+  }
+  getPlayerNameById(id)
+  {
+    for (var k in this.players)
+    {
+      if (this.players.hasOwnProperty(k) && this.players[k].id == id)
+      {
+        return k;
+      }
+    }
+    return undefined;
+  }
+  removePlayer(uid, name)
+  {
+    // move character back to cahracters from used_characters
+    this.characters.push(this.used_characters[0]);
+    this.used_characters.splice(0, 1);
+    delete this.players[name];
+  }
+  addPlayer(uid, name)
+  {
+    this.players[name] = {id: uid, character: this.characters[0], roomid: 0};
+    // move character from characters from used_characters
+    this.used_characters.push(this.characters[0]);
+    this.characters.splice(0, 1);
+  }
+  move(id, dir)
+  {
+    let pl = this.players[this.getPlayerNameById(id)];
+    if (dir == "left")
+    {
+      pl.roomid -= 1;
+    }
+    else if (dir == "right")
+    {
+      pl.roomid += 1;
+    }
+  }
+
+
+  getRooms()
+  {
+    return this.rooms;
+  }
+  getAllCharacters()
+  {
+    return this.all_characters;
+  }
+  getPlaces()
+  {
+    return this.places;
+  }
+  getThings()
+  {
+    return this.weapons;
   }
 };
+
+// export as Game
+module.exports = { Game: Game };
